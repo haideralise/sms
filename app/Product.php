0a9e9074d8a6e8a2b974b579ef3c61f272471fc5
Haider Ali <se.haiderali@gmail.com>
2018-08-15 00:08:52 +0500

    added relations

--- app/Product.php
+++ app/Product.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Models\Purchase;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
@@ -9,4 +10,12 @@ class Product extends Model
     protected $fillable = [
         'name', 'description'
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function purchases()
+    {
+        return $this->hasMany(Purchase::class, 'product_id');
+    }
 }
